70fe69a3bb51c2f44a71fbf40a0bd65208b0a2a2
Younes Benbouchta <younes.benbouchta@student.vinci.be>
2023-11-08 23:32:55 +0100

    Added custom events handling

--- socketServer.js
+++ socketServer.js
@@ -1,9 +1,12 @@
 const { createServer } = require("http");
 const { Server } = require("socket.io");
 const express = require("express");
+import { JsonDB, Config } from 'node-json-db';
 
 const app = express();
 const httpServer = createServer(app);
+const db = new JsonDB(new Config("room_data", true, false, '/'));
+
 const io = new Server(httpServer, {
   cors: {
     origin: "*", // Autorisez toutes les origines
@@ -12,12 +15,34 @@ const io = new Server(httpServer, {
 });
 
 io.on("connection", (socket) => {
+  let joinedRoom = -1;
   console.log("Nouvelle connexion établie");
+
+  const disconnectIfConnected = () => {
+    if (joinedRoom != -1) {
+      socket.leave(`room${joinedRoom}`);
+      joinedRoom = -1;
+    }
+  }
+
+  socket.on("join room", (roomID) => {
+    disconnectIfConnected();
+    socket.join(`room${roomID}`);
+    joinedRoom = roomID;
+  });
+
+  socket.on("todolist element", (message) =>  {
+    db.push(`room${joinedRoom}/`);
+    io.to(`room${joinedRoom}`).emit("todolist element", message); // Diffusez le message à tous les clients connectés
+  });
+
   socket.on("chat message", (message) => {
-    io.emit("chat message", message); // Diffusez le message à tous les clients connectés
+    db.push(`room${joinedRoom}/messages`, [...db.getData(`room${joinedRoom}/messages`), ]);
+    io.to(`room${joinedRoom}`).emit("chat message", message); // Diffusez le message à tous les clients connectés
   });
 
   socket.on("disconnect", () => {
+    disconnectIfConnected();
     console.log("Client déconnecté");
   });
 });
