828ecce078677768b43bfdcbac37b9f4fcfc55cc
Younes Benbouchta <younes.benbouchta@student.vinci.be>
2023-11-11 21:33:41 +0100

    corrected some handlers

--- socketServer.js
+++ socketServer.js
@@ -14,7 +14,6 @@ const io = new Server(httpServer, {
     origin: "*", // Autorisez toutes les origines
   },
 });
-instrument(io, { auth: false });
 
 io.on("connection", (socket) => {
   let joinedRoom = -1;
@@ -30,26 +29,26 @@ io.on("connection", (socket) => {
   socket.on("join room", (room) => {
     console.log("join  ", room);
     socket.join(room);
+    joinedRoom = room;
   });
 
   socket.on("todolist element", (message) => {
-    db.push(`room${joinedRoom}/`);
-    io.to(`room${joinedRoom}`).emit("todolist element", message); // Diffusez le message à tous les clients connectés
+    // db.push(`room${joinedRoom}/`);
+    io.to(joinedRoom).emit("todolist element", message); // Diffusez le message à tous les clients connectés
   });
 
-  socket.on("chat message", (message, room) => {
+  socket.on("chat message", (message) => {
     console.log(message);
-    console.log(room);
-    const defaultMessages =
+    console.log(joinedRoom);
+    /*const defaultMessages =
       db.getObjectDefault(`room${joinedRoom}/messages`, [1, 2, 3]) || [];
-    db.push(`${joinedRoom}/messages`, defaultMessages);
-    socket.to(room).emit("chat message", "ningazazgzae GA KITA");
-    socket.broadcast.emit("chat message", "TOUT LE MONDE");
+    db.push(`${joinedRoom}/messages`, defaultMessages); */
+    io.to(joinedRoom).emit("chat message", message);
   });
 
-  socket.on("update-socket", (toDoList, room) => {
-    console.log(toDoList);
-    socket.to(room).emit("update-socket", toDoList);
+  socket.on("update-todolist", (toDoList) => {
+    console.log(toDoList, joinedRoom);
+    io.to(joinedRoom).emit("updated-todolist", toDoList);
   });
 
   socket.on("disconnect", () => {
@@ -62,3 +61,5 @@ const port = 4001; // Port sur lequel votre serveur Socket.io écoutera
 httpServer.listen(port, () => {
   console.log(`Le serveur Socket.io écoute sur le port ${port}`);
 });
+
+instrument(io, { auth: false });
