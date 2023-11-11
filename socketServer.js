0c01788b935874bc6b6c231e12f40295368e2ccd
hamzarzine2 <hamza.rzine@student.vinci.be>
2023-11-11 17:44:42 +0100

    format code

--- socketServer.js
+++ socketServer.js
@@ -1,20 +1,20 @@
-const {instrument} = require("@socket.io/admin-ui")
+const { instrument } = require("@socket.io/admin-ui");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
 const express = require("express");
-const { JsonDB, Config } = require ('node-json-db');
+const { JsonDB, Config } = require("node-json-db");
 const { log } = require("console");
 
 const app = express();
 const httpServer = createServer(app);
-const db = new JsonDB(new Config("room_data", true, false, '/'));
+const db = new JsonDB(new Config("room_data", true, false, "/"));
 
 const io = new Server(httpServer, {
   cors: {
     origin: "*", // Autorisez toutes les origines
   },
 });
-instrument(io,{auth:false})
+instrument(io, { auth: false });
 
 io.on("connection", (socket) => {
   let joinedRoom = -1;
@@ -25,33 +25,32 @@ io.on("connection", (socket) => {
       socket.leave(`room${joinedRoom}`);
       joinedRoom = -1;
     }
-  }
+  };
 
   socket.on("join room", (room) => {
-    console.log('join  ',room);
+    console.log("join  ", room);
     socket.join(room);
   });
 
-  socket.on("todolist element", (message) =>  {
+  socket.on("todolist element", (message) => {
     db.push(`room${joinedRoom}/`);
     io.to(`room${joinedRoom}`).emit("todolist element", message); // Diffusez le message à tous les clients connectés
   });
 
-  socket.on("chat message", (message,room) => {
+  socket.on("chat message", (message, room) => {
     console.log(message);
     console.log(room);
-    const defaultMessages = db.getObjectDefault(`room${joinedRoom}/messages`, [1, 2, 3]) || [];
-    db.push(`${joinedRoom}/messages`, defaultMessages);    
+    const defaultMessages =
+      db.getObjectDefault(`room${joinedRoom}/messages`, [1, 2, 3]) || [];
+    db.push(`${joinedRoom}/messages`, defaultMessages);
     socket.to(room).emit("chat message", "ningazazgzae GA KITA");
-    socket.broadcast.emit("chat message",'TOUT LE MONDE')
+    socket.broadcast.emit("chat message", "TOUT LE MONDE");
   });
 
-
-  socket.on("update-socket",(toDoList,room) =>{
+  socket.on("update-socket", (toDoList, room) => {
     console.log(toDoList);
-    socket.to(room).emit("update-socket",toDoList);
-  })
-
+    socket.to(room).emit("update-socket", toDoList);
+  });
 
   socket.on("disconnect", () => {
     disconnectIfConnected();
@@ -63,7 +62,3 @@ const port = 4001; // Port sur lequel votre serveur Socket.io écoutera
 httpServer.listen(port, () => {
   console.log(`Le serveur Socket.io écoute sur le port ${port}`);
 });
-
-
-
-
